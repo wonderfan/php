5ffcdeb2669eed95580d9a59f50d376e6b7304f2
阿樊提 <fanjiahe2000@163.com>
2015-04-19 14:42:27 +0800

    Use this as boil-template to create wp plugin

---
+++ wp/plugin-template.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: WP REST API
+ * Description: JSON-based REST API for WordPress, developed as part of GSoC 2013.
+ * Version: 1.2.0
+ * Author: wonderfan
+ * Author URI: http://www.wonderfan.net
+ * Plugin URI: https://github.com/wonderfan/
+ */
