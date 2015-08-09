f749f44c50f1f4f3e5ce1a238c94e4cc4feae1f3
阿樊提 <fanjiahe2000@163.com>
2015-08-09 17:54:56 +0800

    Update template-debug.php

--- WordPress/template-debug.php
+++ WordPress/template-debug.php
@@ -10,3 +10,20 @@
 		var_dump($included_files);
 	?>
 </div>
+
+<?php
+
+function define_current_theme_file( $template ) {
+    $GLOBALS['current_theme_template'] = basename($template);
+    return $template;
+}
+
+add_action('template_include', 'define_current_theme_file', 1000);
+
+function print_template_name(){
+    echo "<em>";
+    echo $GLOBALS['current_theme_template'];
+    echo "</em>";
+}
+
+?>
