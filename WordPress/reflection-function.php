d6fbafb6d7234076a626af2816cb02e5e5995119
阿樊提 <fanjiahe2000@163.com>
2015-08-10 15:04:02 +0800

    Update reflection-function.php

--- WordPress/reflection-function.php
+++ WordPress/reflection-function.php
@@ -1,3 +1,9 @@
-	$reflect = new ReflectionFunction('is_page_template');
-	
-	echo $reflect->getFileName();
+<?php
+
+$reflect = new ReflectionFunction('is_page_template');
+echo $reflect->getFileName();
+
+$lines = file($reflect->getFileName());
+for($l = $reflect->getStartLine(); $l < $reflect->getEndLine(); $l++) {
+	$str .= $lines[$l];
+}
